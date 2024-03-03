f6a2cea5e59f2533491515776bc43e31670762e5
Leela <leelavathy.varadarajan2020@gmail.com>
2024-03-02 18:24:18 -0600

    Initial

--- src/Pattern.java
+++ src/Pattern.java
@@ -1,6 +1,3 @@
-import javax.swing.*;
-import java.util.Scanner;
-
 public class Pattern {
     public static void main(String[] args) {
         System.out.println("Printing the pattern ");
@@ -8,18 +5,19 @@ public class Pattern {
         {
             for (int j=5;j>=1;j--)
             {
+                System.out.println("printing j " +j);
                 if((i+j)>5) {
                     System.out.println(j);
                 }
                 else
 
                 {
+                    System.out.println("printing i in else " +i);
                     System.out.println(5-i);
                 }
             }
             System.out.println();
-
-
+            System.out.println("printing i " +i);
         }
 
 
